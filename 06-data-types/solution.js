24918251d39705f087dee0fa42ec7d4d68ae5488
s1t3w4n <s1t3w4n@yandex.ru>
2023-08-26 23:17:02 +0300

    has solved 06-data-types

---
+++ 06-data-types/solution.js
@@ -0,0 +1,29 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+const stringVar = 'stringVar';
+const numberVar = 123;
+const booleanVar = true;
+const nullVar = null;
+const undefinedVar = undefined;
+const objectVar = {};
+const arrayVar = [];
+console.log(typeof stringVar);
+console.log(typeof numberVar);
+console.log(typeof booleanVar);
+console.log(typeof nullVar);
+console.log(typeof undefinedVar);
+console.log(typeof objectVar);
+console.log(typeof arrayVar);
