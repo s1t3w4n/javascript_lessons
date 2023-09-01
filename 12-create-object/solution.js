fae4cb1ce772a41df42928e53e3f8e3cbe7627f9
s1t3w4n <s1t3w4n@yandex.ru>
2023-09-01 23:05:11 +0300

    has solved 12-create-object

---
+++ 12-create-object/solution.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const object = {
+    "name" : "Aleksey",
+    "surname" : "Bat'kovich",
+    "favoriteNumber" : 8
+}
+
+console.log(`My name is ${object.name} ${object.surname} and my favorite number is ${object.favoriteNumber}`);
