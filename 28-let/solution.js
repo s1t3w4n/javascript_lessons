236dd2f423a32a28dbc2fb039954d8e0c785d166
s1t3w4n <s1t3w4n@yandex.ru>
2023-09-17 22:43:23 +0300

    has solved 28-let

---
+++ 28-let/solution.js
@@ -0,0 +1,16 @@
+/** ЗАДАЧА 28 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+
+// const myFavoriteAnimal = 'Monkey'
+let myFavoriteAnimal = 'Monkey'
+
+console.log(myFavoriteAnimal)
+// 'Monkey'
+
+// const myFavoriteAnimal = 'Cat'
+myFavoriteAnimal = 'Cat'
+
+console.log(myFavoriteAnimal)
+// 'Cat'
